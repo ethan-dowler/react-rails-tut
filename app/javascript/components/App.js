bb7f733cb14230d06534cc3a46db1d3d3c54b619
Ethan Dowler <ethan.dowler@daveramsey.com>
2020-12-11 11:23:38 -0600

    add react Hello World!

---
+++ app/javascript/components/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div className="App">Hello, React!</div>;
+
+export default App;
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,10 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../components/App';
 
-import Rails from "@rails/ujs"
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.querySelector('#main');
+  if (!container) return;
 
-Rails.start()
+  ReactDOM.render(<App />, container);
+});
